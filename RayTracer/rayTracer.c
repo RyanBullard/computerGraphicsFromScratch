2c648161ccfaf098b04bebd1d05af519e7c86dee
RyanBullard <cooldudebullard@gmail.com>
2024-03-20 13:40:19 -0700

    General code cleanup, code comments, and implemented reflections.

--- RayTracer/rayTracer.c
+++ RayTracer/rayTracer.c
@@ -13,85 +13,85 @@
 
 static bool quit = false;
 
-struct {
+struct { // Represents the frame we are drawing to.
     int width;
     int height;
     uint32_t *pixels;
 } frame = { 0 };
 
-typedef struct vec3 {
+typedef struct vec3 { // Represents both a point in space, and a mathematical vector.
     double x;
     double y;
     double z;
 } vec3;
 
-typedef struct rgb {
+typedef struct rgb { // Stores color information in a more readable way compared to a uint32_t.
     uint8_t red;
     uint8_t green;
     uint8_t blue;
 } rgb;
 
-typedef struct ambientLight {
-    double intensity;
-} ambientLight;
-
-typedef struct pointLight {
+typedef struct pointLight { // Represents light emitted from a singular point in the scene. Similar to a light bulb.
     double intensity;
     vec3 pos;
 } pointLight;
 
-typedef struct dirLight {
+typedef struct dirLight { // Represents a light coming from a certain direction. Similar to the sun.
     double intensity;
     vec3 dir;
 } dirLight;
 
-typedef struct sphere {
+typedef struct sphere { // Represents one of the objects in the scene, a sphere.
     vec3 center;
     uint32_t radius;
     rgb color;
     uint32_t specular;
+    double reflectivity;
 } sphere;
 
-typedef struct sphereList {
+typedef struct sphereList { // Holds the list of all objects in the hard coded scene.
     sphere data;
     struct sphereList *next;
 } sphereList;
 
-typedef struct pointLightList {
+typedef struct pointLightList { // Holds a list of all point lights placed in the scene.
     pointLight data;
     struct pointLightList *next;
 } pointLightList;
 
-typedef struct dirLightList {
+typedef struct dirLightList { // Holds a list of all directional lights used in the scene.
     dirLight data;
     struct dirLightList *next;
 } dirLightList;
 
-typedef struct light {
-    ambientLight ambient;
+typedef struct light { // Represents the overall lights in the scene.
+    double ambient;
     dirLightList *dirList;
     pointLightList *pointList;
 } light;
 
-typedef struct sphereResult {
+typedef struct sphereResult { // Used to hold information when determining which sphere is closest to the camera.
     double firstT;
     double secondT;
 } sphereResult;
 
-typedef struct intersectResult {
+typedef struct intersectResult { // Used to hold information about the sphere that may intersect a ray.
     sphere *s;
     double t;
 } intersectResult;
 
-static BITMAPINFO bmi;
-static HBITMAP frameBitmap = 0;
-static HDC fdc = 0;
+static BITMAPINFO bmi; // The header for the bitmap that is drawn to the screen.
+static HBITMAP frameBitmap = NULL; // The pointer to the bitmap we draw.
+static HDC fdc = NULL; // Represents the device context of our frame.
 
-rgb background;
+rgb background; // Holds our background color for the scene.
 
-sphereList sceneList;
-light sceneLight;
+sphereList sceneList; // Global list of objects in the scene.
+light sceneLight; // Global light identifiers.
 
+/*
+ * WindowProcessMessage - Handler to process messages sent from windows to this program.
+ */
 static LRESULT CALLBACK WindowProcessMessage(HWND windowHandle, UINT message, WPARAM wParam, LPARAM lParam) {
     switch (message) {
     case WM_QUIT:
@@ -136,7 +136,7 @@ static LRESULT CALLBACK WindowProcessMessage(HWND windowHandle, UINT message, WP
 
 /*
  * getColor - Converts the color struct to a 32-bit int containing 8 bits of filler, the 8 red bits, the 8 green bits, then the 8
- * blue bits, in that order.
+ * blue bits, in that order. Used to set the appropriate pixel in the bitmap to a color.
  */
 static uint32_t getColor(rgb c) {
     uint32_t compColor = 0;
@@ -174,16 +174,25 @@ static void putPixel(int32_t x, int32_t y, rgb c) {
     frame.pixels[offsetY * frame.width + offsetX] = getColor(c);
 }
 
+/*
+ * canvasToViewport - Converts a screen space coordinate to a coordinate in the 3D view plane.
+ */
 static void canvasToViewport(int x, int y, vec3 *dest) {
     dest->x = x * ((double) VIEWPORT_WIDTH / frame.width);
     dest->y = y * ((double) VIEWPORT_HEIGHT / frame.height);
     dest->z = (double)DISTANCE;
 }
 
+/*
+ * dotProduct - Computes the dot product of two vectors. 
+ */
 static double dotProduct(vec3 vector1, vec3 vector2) {
     return vector1.x * vector2.x + vector1.y * vector2.y + vector1.z * vector2.z;
 }
 
+/*
+ * vecSub - Subtracts two vectors component wise.
+ */
 static vec3 vecSub(vec3 vector1, vec3 vector2) {
     return (vec3) {
         .x = vector1.x - vector2.x,
@@ -192,6 +201,9 @@ static vec3 vecSub(vec3 vector1, vec3 vector2) {
     };
 }
 
+/*
+ * vecAdd - Adds to vectors component wise.
+ */
 static vec3 vecAdd(vec3 vector1, vec3 vector2) {
     return (vec3) {
         .x = vector1.x + vector2.x,
@@ -200,18 +212,28 @@ static vec3 vecAdd(vec3 vector1, vec3 vector2) {
     };
 }
 
-static vec3 vecConstMul(double constant, vec3 *vector) {
+/*
+ * vecConstMul - Multiplies each component of a vector by a constant.
+ */
+static vec3 vecConstMul(double constant, vec3 vector) {
     return (vec3) {
-        .x = constant * vector->x,
-        .y = constant * vector->y,
-        .z = constant * vector->z
+        .x = constant * vector.x,
+        .y = constant * vector.y,
+        .z = constant * vector.z
     };
 }
 
+/*
+ * magnitude - Computes the magnitude of a 3D vector.
+ */
 static double magnitude(vec3 vector) {
     return sqrt((vector.x * vector.x) + (vector.y * vector.y) + (vector.z * vector.z));
 }
 
+/*
+ * normalize - Returns the normalized vector of the passed in vector. That is - each component is divided
+ * by the overall magnitude of the vector.
+ */
 static vec3 normalize(vec3 vector) {
     double mag = magnitude(vector);
     return (vec3) {
@@ -221,6 +243,16 @@ static vec3 normalize(vec3 vector) {
     };
 }
 
+/*
+ * reflectRay - Reflects a ray with respect to a normal.
+ */
+static vec3 reflectRay(vec3 ray, vec3 normal) {
+    return vecSub(vecConstMul((2 * dotProduct(normal, ray)), normal), ray);
+}
+
+/*
+ * colorMul - Multiplies a color by a constant. This function clamps the color down to 255.
+ */
 static rgb colorMul(rgb *color, double mul) {
     double red = color->red * mul;
     double green = color->green * mul;
@@ -247,6 +279,39 @@ static rgb colorMul(rgb *color, double mul) {
     return (rgb) { .red = redComp, .blue = blueComp, .green = greenComp };
 }
 
+/*
+ * colorMul - Multiplies a color by a constant. This function clamps the color down to 255.
+ */
+static rgb colorAdd(rgb color, rgb color2) {
+    uint32_t red = color.red + color2.red;
+    uint32_t green = color.green + color2.green;
+    uint32_t blue = color.blue + color2.blue;
+
+    uint8_t redComp = 0;
+    if (red < 255) {
+        redComp = (uint8_t)red;
+    } else {
+        redComp = 255;
+    }
+    uint8_t greenComp = 0;
+    if (green < 255) {
+        greenComp = (uint8_t)green;
+    } else {
+        greenComp = 255;
+    }
+    uint8_t blueComp = 0;
+    if (blue < 255) {
+        blueComp = (uint8_t)blue;
+    } else {
+        blueComp = 255;
+    }
+    return (rgb) { .red = redComp, .blue = blueComp, .green = greenComp };
+}
+
+/*
+ * intersectRaySphere - This function finds the closest sphere that intersects a given ray.
+ * A result of DBL_MAX means that no sphere intersects this ray at any point.
+ */
 static sphereResult intersectRaySphere(vec3 *origin, vec3 *direction, sphere *s) {
     uint32_t radius = s->radius;
     vec3 offsetO = vecSub(*origin, s->center);
@@ -265,6 +330,10 @@ static sphereResult intersectRaySphere(vec3 *origin, vec3 *direction, sphere *s)
     return (sphereResult) { .firstT = t1, .secondT = t2 };
 }
 
+/*
+ * closestIntersection - Finds the closest sphere to a point that intersects a given vector. If the .s field of the
+ * returned intersectResult struct is NULL, then no sphere intersects this vector.
+ */
 static intersectResult closestIntersection(vec3 *origin, vec3 *D, double t_min, double t_max) {
     double closestT = DBL_MAX;
     sphere *closestSphere = NULL;
@@ -284,9 +353,12 @@ static intersectResult closestIntersection(vec3 *origin, vec3 *D, double t_min, 
     return (intersectResult) { .s = closestSphere, .t = closestT };
 }
 
+/*
+ * computeLighting - Computes the intensity of lighting at a certain point in the scene.
+ */
 static double computeLighting(vec3 *point, vec3 *normal, vec3 v, uint32_t spec) {
     double intensity = 0.0;
-    intensity += sceneLight.ambient.intensity;
+    intensity += sceneLight.ambient;
     for (dirLightList *dLightNode = sceneLight.dirList; dLightNode != NULL; dLightNode = dLightNode->next) {
         double nDotL = dotProduct(*normal, dLightNode->data.dir);
 
@@ -301,7 +373,7 @@ static double computeLighting(vec3 *point, vec3 *normal, vec3 v, uint32_t spec) 
         }
 
         if (spec != -1) {
-            vec3 r = vecSub(vecConstMul(2 * nDotL, normal), dLightNode->data.dir);
+            vec3 r = reflectRay(dLightNode->data.dir, *normal);
             double rDotV = dotProduct(r, v);
             if (rDotV > 0) {
                 intensity += dLightNode->data.intensity * pow(rDotV / (magnitude(r) * magnitude(v)), spec);
@@ -324,7 +396,7 @@ static double computeLighting(vec3 *point, vec3 *normal, vec3 v, uint32_t spec) 
         }
 
         if (spec != -1) {
-            vec3 r = vecSub(vecConstMul(2 * nDotL, normal), pointNorm);
+            vec3 r = reflectRay(pointNorm, *normal);
             double rDotV = dotProduct(r, v);
             if (rDotV > 0) {
                 intensity += pLightNode->data.intensity * pow(rDotV / (magnitude(r) * magnitude(v)), spec);
@@ -335,7 +407,10 @@ static double computeLighting(vec3 *point, vec3 *normal, vec3 v, uint32_t spec) 
     return intensity;
 }
 
-static rgb traceRay(vec3 *origin, vec3 *D, uint32_t t_min, uint32_t t_max) {
+/*
+ * traceRay - Follows a ray from the view plane into the scene, and finds the color that needs to be plotted.
+ */
+static rgb traceRay(vec3 *origin, vec3 *D, double t_min, double t_max, uint32_t depth) {
     intersectResult res = closestIntersection(origin, D, t_min, t_max);
 
     sphere *closestSphere = res.s;
@@ -344,13 +419,29 @@ static rgb traceRay(vec3 *origin, vec3 *D, uint32_t t_min, uint32_t t_max) {
     if (closestSphere == NULL) {
         return background;
     }
-    vec3 p = vecAdd(*origin, vecConstMul(closestT, D));
+    vec3 p = vecAdd(*origin, vecConstMul(closestT, *D));
     vec3 normal = vecSub(p, closestSphere->center);
     normal = normalize(normal);
-    return colorMul(&closestSphere->color, computeLighting(&p, &normal, vecConstMul(-1, D), closestSphere->specular));
+    vec3 view = vecConstMul(-1, *D);
+    rgb localColor = colorMul(&closestSphere->color, computeLighting(&p, &normal, view, closestSphere->specular));
+
+    double r = closestSphere->reflectivity;
+    if (depth == 0 || r <= 0.0) {
+        return localColor;
+    }
+
+    vec3 ray = reflectRay(view, normal); // Get the ray we are looking out of from the surface of the object
+
+    rgb reflectedColor = traceRay(&p, &ray, 0.001, DBL_MAX, depth - 1);
+
+    return colorAdd(colorMul(&localColor, 1 - r), colorMul(&reflectedColor, r));
 }
 
+/*
+ * renderScene - Does the needed setup, then calls the required functions to render the raytraced scene.
+ */
 static void renderScene() {
+    uint32_t recursionDepth = 3;
     vec3 O = (vec3){ 0 };
     O.x = 0;
     O.y = 0;
@@ -360,12 +451,15 @@ static void renderScene() {
             vec3 D;
             canvasToViewport(x, y, &D);
             rgb c;
-            c = traceRay(&O, &D, DISTANCE, INT32_MAX);
+            c = traceRay(&O, &D, DISTANCE, DBL_MAX, recursionDepth);
             putPixel(x, y, c);
         }
     }
 }
 
+/*
+ * WinMain - The main function of a win32 program. Sets up the graphical scene then begins the rendering process.
+ */
 int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow) {
 
     if (!AllocConsole()) { // If the call fails return
@@ -399,25 +493,29 @@ int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
     sphere red = (sphere){ .center = (vec3){.x = 0.0, .y = -1.0, .z = 3.0},
         .radius = 1,
         .color = (rgb) {.red = 255, .green = 0, .blue = 0 },
-        .specular = 500
+        .specular = 500,
+        .reflectivity = 0.2
     };
 
     sphere blue = (sphere){ .center = (vec3){.x = 2.0, .y = 0.0, .z = 4.0},
         .radius = 1,
         .color = (rgb) {.red = 0, .green = 0, .blue = 255 },
-        .specular = 500
+        .specular = 500,
+        .reflectivity = 0.3
     };
 
     sphere green = (sphere){ .center = (vec3){.x = -2.0, .y = 0.0, .z = 4.0},
         .radius = 1,
         .color = (rgb) {.red = 0, .green = 255, .blue = 0 },
-        .specular = 10
+        .specular = 10,
+        .reflectivity = 0.4
     };
 
     sphere yellow = (sphere){ .center = (vec3){.x = 0.0, .y = -5001.0, .z = 0.0},
         .radius = 5000,
         .color = (rgb) {.red = 255, .green = 255, .blue = 0 },
-        .specular = 1000
+        .specular = 1000,
+        .reflectivity = 0.5
     };
 
     sceneList.data = red;
@@ -433,19 +531,18 @@ int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
     third.next = &fourth;
     fourth.next = NULL;
 
-    background.red = 255;
-    background.green = 255;
-    background.blue = 255;
+    background.red = 0;
+    background.green = 0;
+    background.blue = 0;
 
     //Build our list of lights in the scene
-    ambientLight aLight = (ambientLight){ .intensity = 0.2 };
     pointLight pLight = (pointLight){ .intensity = 0.6, .pos = (vec3){ .x=2.0, .y = 1.0, .z = 0.0} };
     dirLight dLight = (dirLight){ .intensity = 0.2, .dir = (vec3){.x = 1.0, .y = 4.0, .z = 4.0} };
 
     dirLightList dList = (dirLightList){ .data = dLight, .next = NULL };
     pointLightList pList = (pointLightList){ .data = pLight, .next = NULL };
 
-    sceneLight = (light){ .ambient = aLight, .dirList = &dList, .pointList = &pList };
+    sceneLight = (light){ .ambient = 0.2, .dirList = &dList, .pointList = &pList };
 
     while (!quit) {
         static MSG message = { 0 };
